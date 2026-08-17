78788d2a670a957d5c268b97adaa5d8175c2aa5f
praveenbhosage-tenarai <praveen.bhosage@tenarai.com>
2026-08-17 06:28:32 -0400

    Updated employee model

--- models/employee.js
+++ models/employee.js
@@ -1,1 +1,15 @@
-
+const mongoose = require('mongoose');
+mongoose.connect(
+  'mongodb://localhost:27017/Employees,
+  { useUnifiedTopology: true },
+  { useNewUrlParser: true}
+);
+const empSchema = new mongoose.Schema({
+  empName: String,
+  empPass: String,
+  created: {type: Date, default: Date.now }
+},{
+    collection:'FTEmployees'
+});
+// 
+module.exports = mongoose.model('Employees', empSchema);
